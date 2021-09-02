43a9a0e71c9ffeeda30c0be8ef255e787319992d
cinjon <cinjon.resnick@gmail.com>
2021-09-02 16:30:46 -0400

    update on the mapping.

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -5,7 +5,7 @@ import {
   Unit4,
   Transfer
 } from "../generated/Unit4/Unit4"
-import { User } from "../generated/schema"
+import { Unit, User, UserUnit } from "../generated/schema"
 
 export function handleTransfer(event: Transfer): void {
   let fromAddress = event.params.from;
@@ -15,30 +15,50 @@ export function handleTransfer(event: Transfer): void {
 
   let transferAddress = event.address;
   let transferAddressString = transferAddress.toHexString();
+  let fromUserUnitID = fromString + "-" + transferAddressString;
+  let toUserUnitID = toString + "-" + transferAddressString;  
+
+  let unit = Unit.load(transferAddressString);
+  if (unit == null) {
+    unit = new Unit(transferAddressString);
+    unit.createdAt = event.block.timestamp;
+    unit.address = transferAddress;
+  }
+  unit = unit as Unit;
 
   let fromUser = User.load(fromString);
+  let fromUserUnit = UserUnit.load(fromUserUnitID);
+
   let updateFrom = false;
   let zeroAddress = new Address(0);
   if (fromAddress != zeroAddress && fromUser != null) {
     // We are not minting.
-    fromUser = fromUser as User;
-    fromUser.balance = fromUser.balance.minus(amount);
+    fromUserUnit.balance = fromUserUnit.balance.minus(amount);
     updateFrom = true;
-  } 
+  }
   
   let toUser = User.load(toString);
+  let toUserUnit = UserUnit.load(toUserUnitID);
   if (toUser == null) {
     toUser = new User(toString);
     toUser.address = event.params.to;
     toUser.createdAt = event.block.timestamp;
-    toUser.balance = BigInt.fromI32(0);
   }
   toUser = toUser as User;
   
-  toUser.balance = toUser.balance.plus(amount);
+  if (toUserUnit == null) {
+    toUserUnit = new UserUnit(toUserUnitID);
+    toUserUnit.user = toString;
+    toUserUnit.unit = transferAddressString;
+    toUserUnit.balance = BigInt.fromI32(0);
+  }
+  toUserUnit.balance = toUserUnit.balance.plus(amount);
+
   if (updateFrom) {
-    fromUser.save();
+    fromUserUnit.save();
   }
   toUser.save();
+  toUserUnit.save();
+  unit.save();
 }
 
